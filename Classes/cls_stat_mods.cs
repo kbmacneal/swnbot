8f2896b923ff8ef3d16893e45a84593f1e9e10a3
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-09-09 18:55:23 -0500

    added stat mod gen

---
+++ Classes/cls_stat_mods.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Collections;
+
+namespace swnbot.Classes
+
+{
+    public class stat_mod
+    {
+        public int value {get;set;}
+        public int mod {get;set;}
+
+        public List<stat_mod> gen_mods()
+        {
+            List<stat_mod> rtn = new List<stat_mod>();
+
+            for (int i = 1; i < 19; i++)
+            {
+                if(i<=3)
+                {
+                    stat_mod mod = new stat_mod();
+                    mod.value = i;
+                    mod.mod = -2;
+                    rtn.Add(mod);
+                }
+
+                if(i>=4&&i<=7)
+                {
+                    stat_mod mod = new stat_mod();
+                    mod.value = i;
+                    mod.mod = -1;
+                    rtn.Add(mod);
+                }
+
+                if(i>=8&&i<=13)
+                {
+                    stat_mod mod = new stat_mod();
+                    mod.value = i;
+                    mod.mod = 0;
+                    rtn.Add(mod);
+                }
+                
+                if(i>=14&&i<=17)
+                {
+                    stat_mod mod = new stat_mod();
+                    mod.value = i;
+                    mod.mod = 1;
+                    rtn.Add(mod);
+                }
+                
+                if(i==18)
+                {
+                    stat_mod mod = new stat_mod();
+                    mod.value = i;
+                    mod.mod = 2;
+                    rtn.Add(mod);
+                }
+            }
+
+
+            return rtn;
+        }
+    }
+
+}
