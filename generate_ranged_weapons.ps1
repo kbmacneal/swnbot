99f17fe6240a169dc61d83b9273db423a921d5c7
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-09-09 18:42:55 -0500

    setting up

---
+++ generate_ranged_weapons.ps1
@@ -0,0 +1,14 @@
+class skill {
+    [int]$ID
+    [string]$name
+    [int]$damage
+    [string]$range
+    [int]$cost
+    [string]$magazine
+    [string]$attribute
+    [int]$encumberance
+    [int]$tech_level
+    
+}
+
+$collection = New-Object System.Collections.ArrayList
