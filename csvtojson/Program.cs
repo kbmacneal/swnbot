73673aea2a0870b169c9a69c870626667eb805f0
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-09-10 14:24:48 -0500

    adding helper executable to convert tables to their json representations

---
+++ csvtojson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csvtojson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string csv_path = args[0].ToString();
+        }
+    }
+}
