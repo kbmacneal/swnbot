239cdc26b36383a8401428834d31e9015752f84c
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-09-11 11:27:35 -0500

    moving roll to to subfuction to allow other parts of the code to use it independently

--- Commands/Roll.cs
+++ Commands/Roll.cs
@@ -19,26 +19,21 @@ namespace swnbot.Commands
 
     public class roller : ModuleBase<SocketCommandContext>
     {
-        public static int singleRoll(String roll)
-        {
-            int num_dice = 1;
-            int result = 0;
-            int di = roll.IndexOf('d');
-            if (di == -1) return Int32.Parse(roll);
-            int diceSize = Int32.Parse(roll.Substring(di + 1)); //value of string after 'd'
-            if (di != 0) num_dice = Int32.Parse(roll.Substring(0, di));
-
-            for (int i = 0; i < num_dice; i++)
-            {
-                result += swnbot.Program.rand.Next(0, diceSize) + 1;
-            }
-            return result;
-        }
-
         [Command("roll")]
-        public async Task RollAsync(params string[] args)
+        private async Task RollAsync(params string[] args)
         {
             string roll = string.Join("", args).Replace(" ", "");
+            List<int> dice_results = Roll(roll);
+
+            SocketGuildUser usr = Context.Guild.GetUser(Context.Message.Author.Id) as SocketGuildUser;
+
+            string rtn_name = usr.Nickname == null ? usr.Username : usr.Nickname;
+
+            await ReplyAsync(rtn_name + " rolled a " + dice_results.Sum() + " (" + string.Join(", ",dice_results) + ")");
+            
+        }
+        public static List<int> Roll(string roll)
+        {
             List<int> dice_results = new List<int>();
 
             DiceBag db = new DiceBag();
@@ -94,12 +89,8 @@ namespace swnbot.Commands
                 
             }
             
-
-            SocketGuildUser usr = Context.Guild.GetUser(Context.Message.Author.Id) as SocketGuildUser;
-
-            string rtn_name = usr.Nickname == null ? usr.Username : usr.Nickname;
-
-            await ReplyAsync(rtn_name + " rolled a " + dice_results.Sum() + " (" + string.Join(", ",dice_results) + ")");
+            return dice_results;
+            
         }
     }
 
