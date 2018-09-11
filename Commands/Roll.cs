7ad1e1c4d1904328f4b7d90ba3c73ad4cb3789cf
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-09-11 11:28:32 -0500

    cleaning up formatting

--- Commands/Roll.cs
+++ Commands/Roll.cs
@@ -29,8 +29,8 @@ namespace swnbot.Commands
 
             string rtn_name = usr.Nickname == null ? usr.Username : usr.Nickname;
 
-            await ReplyAsync(rtn_name + " rolled a " + dice_results.Sum() + " (" + string.Join(", ",dice_results) + ")");
-            
+            await ReplyAsync(rtn_name + " rolled a " + dice_results.Sum() + " (" + string.Join(", ", dice_results) + ")");
+
         }
         public static List<int> Roll(string roll)
         {
@@ -38,59 +38,63 @@ namespace swnbot.Commands
 
             DiceBag db = new DiceBag();
 
-            if(roll.Contains('-') || roll.Contains('+'))
+            if (roll.Contains('-') || roll.Contains('+'))
             {
                 char[] splits = new char[] { '+', '-' };
                 string[] two_parts = roll.Split(splits);
                 int mod = 0;
-                if(roll.Contains('-'))
+                if (roll.Contains('-'))
                 {
-                    mod = -1* Convert.ToInt32(two_parts[1]);
+                    mod = -1 * Convert.ToInt32(two_parts[1]);
                 }
-                else{
+                else
+                {
                     mod = Convert.ToInt32(two_parts[1]);
                 }
                 string[] parameters = two_parts[0].Split('d');
                 uint num_dice = 1;
                 int dice_sides = 2;
 
-                if(parameters[0] != "")
+                if (parameters[0] != "")
                 {
                     num_dice = Convert.ToUInt32(parameters[0]);
                     dice_sides = Convert.ToInt32(parameters[1]);
                 }
-                else{
+                else
+                {
                     dice_sides = Convert.ToInt32(parameters[1]);
                 }
 
                 DiceBag.Dice dice = (DiceBag.Dice)System.Enum.Parse(typeof(DiceBag.Dice), dice_sides.ToString());
 
-                dice_results = db.RollQuantity( dice , num_dice );
+                dice_results = db.RollQuantity(dice, num_dice);
 
                 dice_results.Add(mod);
             }
-            else{
+            else
+            {
                 string[] parameters = roll.Split('d');
                 uint num_dice = 1;
                 int dice_sides = 2;
 
-                if(parameters[0] != "")
+                if (parameters[0] != "")
                 {
                     num_dice = Convert.ToUInt32(parameters[0]);
                     dice_sides = Convert.ToInt32(parameters[1]);
                 }
-                else{
+                else
+                {
                     dice_sides = Convert.ToInt32(parameters[1]);
                 }
 
                 DiceBag.Dice dice = (DiceBag.Dice)System.Enum.Parse(typeof(DiceBag.Dice), dice_sides.ToString());
 
-                dice_results = db.RollQuantity( dice , num_dice );
-                
+                dice_results = db.RollQuantity(dice, num_dice);
+
             }
-            
+
             return dice_results;
-            
+
         }
     }
 
